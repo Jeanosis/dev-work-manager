8405655baee9f0e25c6b67c409c70fd925fd5525
Sen <sestritsa.chifuyu@gmail.com>
2015-02-28 17:41:41 +0200

    Classes

---
+++ src/DevWorkMainProject/Contact.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevWorkMainProject
+{
+    enum ContactType
+    {
+        Meet,
+        Call
+
+    }
+    class Contact
+    {
+        private ContactType type;
+        private DateTime date;
+        private string comment;
+        public ContactType Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+        public string Comment
+        {
+            get { return comment; }
+            set { comment = value; }
+        }
+    }
+}
